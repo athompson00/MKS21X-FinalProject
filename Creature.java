f3445035d205d801a760fa61423b48b5edf44515
athompson00 <athompson00@stuy.edu>
2019-01-04 10:57:57 -0500

    created movement functions and constructor for Creature

---
+++ Creature.java
@@ -0,0 +1,47 @@
+//API : http://mabe02.github.io/lanterna/apidocs/2.1/
+import com.googlecode.lanterna.terminal.Terminal.SGR;
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.Key.Kind;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.Terminal.Color;
+import com.googlecode.lanterna.terminal.TerminalSize;
+import com.googlecode.lanterna.LanternaException;
+import com.googlecode.lanterna.input.CharacterPattern;
+import com.googlecode.lanterna.input.InputDecoder;
+import com.googlecode.lanterna.input.InputProvider;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+
+public class Creature{
+  private int health;
+  private int damage;
+  private int xcor;
+  private int ycor;
+  //Basic Creature constructor intakes health and damage and creates the creature on the terminal
+  public Creature(int h, int d, int x, int y){
+    health = h;
+    damage = d;
+    xcor = x;
+    ycor = y;
+  }
+
+//Basic movement functions for all creatures
+  public void moveLeft(){
+    xcor -= 1;
+  }
+
+  public void moveRight(){
+    xcor += 1;
+  }
+
+  public void moveUp(){
+    ycor += 1;
+  }
+
+  public void moveDown(){
+    ycor -= 1;
+  }
+
+
+}
