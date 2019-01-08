cc9edae3840545b1db75a7ee5de7c456ed03a071
stefansor <ssorobay00@stuy.edu>
2019-01-08 10:24:43 -0500

    deleteted imports

--- Creature.java
+++ Creature.java
@@ -1,32 +1,18 @@
-//API : http://mabe02.github.io/lanterna/apidocs/2.1/
-import com.googlecode.lanterna.terminal.Terminal.SGR;
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.Key.Kind;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.Terminal.Color;
-import com.googlecode.lanterna.terminal.TerminalSize;
-import com.googlecode.lanterna.LanternaException;
-import com.googlecode.lanterna.input.CharacterPattern;
-import com.googlecode.lanterna.input.InputDecoder;
-import com.googlecode.lanterna.input.InputProvider;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.KeyMappingProfile;
-
 public class Creature{
   private int health;
   private int damage;
   private int xcor;
   private int ycor;
   private String name;
-  private String graphic;
+  private char graphic;
   //Basic Creature constructor intakes health and damage and creates the creature on the terminal
-  public Creature(int h, int d, int x, int y, String n){
+  public Creature(int h, int d, int x, int y, String n, char graph){
     health = h;
     damage = d;
     xcor = x;
     ycor = y;
     name = n;
+    graphic = graph;
   }
 
 //Basic movement functions for all creatures
@@ -69,11 +55,11 @@ public class Creature{
     return name;
   }
 // change methods for private variables
-  public void changeDamage(d){
+  public void changeDamage(int d){
     damage = d;
   }
 
-  public void changeHealth(h){
+  public void changeHealth(int h){
     health = h;
   }
 
