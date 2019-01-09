64ea94378d73121eeaa2f6afe4da19ea1952339f
stefansor <ssorobay00@stuy.edu>
2019-01-08 23:37:57 -0500

    added direction variable to move funciton

--- Player.java
+++ Player.java
@@ -1,8 +1,8 @@
 public class Player extends Creature{
   private int babiesToCollect; //Will always satrt at 3 or more
   private int babiesCollected; //Will always staart at 0
-  public Player(int h, int d, int x, int y, String n, char graph, babiesToCollect){
-    super(int h, int d, int x, int y, String n, char graph);//defines inherited variables
+  public Player(int h, int d, int x, int y, String n, char graph, int babiesToCollect){
+    super(h, d, x, y, n, graph);//defines inherited variables
     this.babiesToCollect = babiesToCollect;
     babiesCollected = 0;
   }
