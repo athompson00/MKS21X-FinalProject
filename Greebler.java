991b775b0747daf76557af2261800c185e34294e
stefansor <ssorobay00@stuy.edu>
2019-01-08 23:37:13 -0500

    nothing changed

--- Greebler.java
+++ Greebler.java
@@ -1,7 +1,7 @@
 public class Greebler extends Creature{
   private double dodgechance;
   public Greebler(int x, int y){
-    super(100, 10, x, y, "Greebler", '\u262d');
+    super(100, 10, x, y, "Greebler", '\u20AA');
     dodgechance = 0.05;
   }
   public void attack(){
