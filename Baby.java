a748d15eeb8d076e963cd9011fff61394a9332bc
athompson00 <athompson00@stuy.edu>
2019-01-10 10:42:48 -0500

    Baby class created

---
+++ Baby.java
@@ -0,0 +1,10 @@
+public class Baby{
+  private int x;
+  private int y;
+  private boolean isPickedUp;
+  public baby(int xcor, int ycor){
+    x = xcor;
+    y = ycor;
+    isPickedUp = false;
+  }
+}
