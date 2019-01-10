b53a023876bad55b01faffe7c654365499bdb242
athompson00 <athompson00@stuy.edu>
2019-01-09 19:23:47 -0500

    Few changes

--- Greebler.java
+++ Greebler.java
@@ -1,10 +1,34 @@
+import Java.util.Random;
 public class Greebler extends Creature{
-  private double dodgechance;
-  public Greebler(int x, int y){
-    super(100, 10, x, y, "Greebler", '\u262d');
-    dodgechance = 0.05;
+  private int dodgeChance;
+  //simple constructor
+  public Greebler(int h, int d, int x, int y, String n, double c){
+    super(int h, int d, int x, int y, String n);
+    dodgeChance = c;
   }
+  //used to modify damage variable
+  public void changeDamage(newDamage){
+    damage = newDamage;
+  }
+  //returns true if dodge is successful and false if not
+  public boolean Dodge(){
+    int n = java.util.Random.nextInt(100);
+    if (dodgeChance > n){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  //If greebler is attacked, this determines whether or not they take damage
+  public void attacked(int d){
+    if (!this.Dodge()){
+      health = health - d;
+    }
+  }
+
+  //attack player
   public void attack(){
+
   }
 
 }
