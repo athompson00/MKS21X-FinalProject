459158bb9fa2199688bced3e7fe7d862daef4807
stefansor <ssorobay00@stuy.edu>
2019-01-11 02:21:06 -0500

    had to capitalize constructor

--- Baby.java
+++ Baby.java
@@ -2,7 +2,7 @@ public class Baby{
   private int x;
   private int y;
   private boolean isPickedUp;
-  public baby(int xcor, int ycor){
+  public Baby(int xcor, int ycor){
     x = xcor;
     y = ycor;
     isPickedUp = false;
