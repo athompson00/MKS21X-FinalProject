a2860ded8d14cd008af8973a80dc1481868d2004
athompson00 <athompson00@stuy.edu>
2019-01-12 18:02:42 -0500

    testing with terminal size

--- Demo.java
+++ Demo.java
@@ -375,9 +375,9 @@ public static void pickUpBaby(Player n, Terminal t){
   public static void main(String[] args){
     Terminal terminal = TerminalFacade.createTextTerminal();
     terminal.enterPrivateMode();
-    TerminalSize size; //= terminal.getTerminalSize();
-    size.setRows(100);
-    size.setColumns(200);
+    TerminalSize size =  new TerminalSize(100,200);//terminal.getTerminalSize();
+  //  size.setRows(100);
+  //  size.setColumns(200);
     terminal.setCursorVisible(false);
     boolean running = true;
 
