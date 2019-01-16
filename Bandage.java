513b164e31e0e95f7a18b2fe4a6f615519c80a55
stefansor <ssorobay00@stuy.edu>
2019-01-16 11:02:50 -0500

    added class to heal player in the game must show graphic and allow player to pick it up still

---
+++ Bandage.java
@@ -0,0 +1,39 @@
+public class Bandage{
+  private int x;
+  private int y;
+  private boolean isPickedUp;
+  private char graphic;
+  public Bandage(int xcor, int ycor){
+    x = xcor;
+    y = ycor;
+    isPickedUp = false;
+    graphic = '\u1196';
+  }
+  public int getX(){
+    return x;
+  }
+  public int getY(){
+    return y;
+  }
+  public void setX(int xcor){
+    x = xcor;
+  }
+  public void setY(int ycor){
+    y = ycor;
+  }
+  public boolean isPickedUp(){
+    return isPickedUp;
+  }
+
+  public char getGraphic(){
+    return graphic;
+  }
+
+  public void changeGraphic(char n){
+    graphic = n;
+  }
+  //Pickup method enables baby to be picked up
+  public void pickUp(){
+    isPickedUp = true;
+  }
+}
