081035e879c87a0b12fd48539a6c3c92c7714f75
stefansor <ssorobay00@stuy.edu>
2019-01-19 14:17:10 -0500

    player can now level with an increase of damage and a small increase in health

--- Player.java
+++ Player.java
@@ -1,10 +1,12 @@
 public class Player extends Creature{
   private int babiesToCollect; //Will always satrt at 3 or more
   private int babiesCollected; //Will always staart at 0
+  private int level;
   public Player(int h, int d, int x, int y, String n, char graph, int babiesToCollect){
     super(h, d, x, y, n, graph);//defines inherited variables
     this.babiesToCollect = babiesToCollect;
     babiesCollected = 0;
+    level = 1;
   }
   //getMethod for babiesCollected and babiesToCollect
   public int getBabiesCollected(){
@@ -17,5 +19,13 @@ public class Player extends Creature{
   public void pickUpBaby(){
     babiesToCollect--;
     babiesCollected++;
+  }
+  public int getLevel(){
+    return level;
+  }
+  public void levelUp(){
+    level++;
+    this.addHealth(50);
+    this.setDamage(this.getDamage() + 10);
   }
 }
